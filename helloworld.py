5015ccafb7d8238a6200cf9db7530e9eeb9b6d64
BlackDemonXD <83661905+BlackDemonXD@users.noreply.github.com>
2022-03-14 12:29:57 +0600

    Added helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+#Day one of 30 days of python
+
+print (2+3)   #addition
+print (3-1)   #substraction
+print (6/14)  #division
+print (4*8)   #multiplication
+print (9//14) #removes remainder
+print (9**8)  #exponent
