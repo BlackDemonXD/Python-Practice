dba777524b2258101d26ec702d84433dc34157e9
BlackDemonXD <radidraiyan@gmail.com>
2022-03-14 12:36:37 +0600

    Modified helloworld.py to get results

--- helloworld.py
+++ helloworld.py
@@ -6,3 +6,13 @@ print (6/14)  #division
 print (4*8)   #multiplication
 print (9//14) #removes remainder
 print (9**8)  #exponent
+
+#checking data types
+print(type(10))
+print(type(3.14))
+print(type(1+3j))
+print(type('Something'))
+print(type([1, 2, 3]))
+print(type({'name':'something'}))
+print(type({4.8, 9.7, 18.2}))
+print(type((4.8, 9.7, 18.2)))
