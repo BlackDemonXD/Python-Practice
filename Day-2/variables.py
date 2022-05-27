02bfc6774c7a46064eb2d7c08d0d692059da8bd2
BlackDemonXD <radidraiyan@gmail.com>
2022-05-27 15:49:08 +0600

    Created and edited variables.py to include Excercise Level 2

---
+++ Day-2/variables.py
@@ -0,0 +1,14 @@
+#30 days of python programming-2
+
+first_name = 'Ben'
+last_name = 'Dover'
+full_name = 'Ben Dover'
+country = 'deezland'
+city = 'Old zealand'
+age = '69'
+year = '69420'
+is_married = False
+is_true = False
+is_light_on = True
+
+first_name, last_name, age, is_married = 'Ben', 'Dover','69', 'False'
