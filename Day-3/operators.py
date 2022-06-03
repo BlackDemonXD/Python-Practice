e77769df6e75460db489e838cca57405638211d8
BlackDemonXD <radidraiyan@gmail.com>
2022-06-03 22:44:34 +0600

    Reformatted previous texts to make the code look cleaner

--- Day-3/operators.py
+++ Day-3/operators.py
@@ -1,14 +1,17 @@
 age = 16
 height = 176.0
 complex_number = '1+4j'
+
+#Area of a Rectangle
 print("Area of the triangle: ")
 base = int(input('base: '))
 height = int(input('height: '))
+
 Area = base*height*0.5
 print("Area of the triangle is", Area)
 
 #Perimeter Of a Triangle
-
+print("Perimeter of the Triangle: ")
 side_a = int(input('a: '))
 side_b = int(input('b: '))
 side_c = int(input('c: '))
