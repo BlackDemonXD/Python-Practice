5fc6641fe61f84dac5f115cd88e78ed13be863e5
BlackDemonXD <radidraiyan@gmail.com>
2022-06-17 22:14:35 +0600

    Finished upto task 17

--- Day-3/operators.py
+++ Day-3/operators.py
@@ -30,3 +30,21 @@ print("The Area of the Rectangle is", Area_Rectangle)
 print("Perimeter of the Rectangle: ")
 Perimeter_Rectangle = 2*rectangle_base*rectangle_length
 print("Perimeter of the Rectangle is", Perimeter_Rectangle)
+
+print("Radius of a circle: ")
+r = int(input('radius: '))
+Circumference = 2*3.1416*r
+print("Circumference of the circle is", Circumference)
+
+print((len('python') != (len('dragon'))))
+print('on' in 'python' and 'on' in 'dragon')
+print('jargon' in 'I hope this course is not full of jargon')
+print('on' not in 'dragon' and 'jargon')
+print(len('python'))
+string = '6'
+print(string)
+float = 6.0
+print(float)
+
+num = int(input('number: '))
+print("The number is even:", num%2 == 0)
